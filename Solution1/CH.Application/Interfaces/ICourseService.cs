2db368550b087aa256a9d7c5a634cb6dc5e8773c
cheehong <cheehongtang@hotmail.com>
2020-12-13 17:26:19 +0800

    Commit service and repository interfaces, CourseViewModel

---
+++ Solution1/CH.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CH.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CH.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+
+    }
+}
---
+++ Solution1/CH.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CH.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CH.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        IEnumerable<Course> Courses { get; set; }
+    }
+}
