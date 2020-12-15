d41adfa3d209c9fb1ccacbd4811c2ba4ef5d0730
cheehong <cheehongtang@hotmail.com>
2020-12-15 11:05:28 +0800

    Commit presentation layer, styling, and authentication

---
+++ Solution1/CH.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CH.Application.Interfaces;
+using CH.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CH.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
