17e1218765686e6cd3ff7c26668a9dd62deb6a22
cheehong <cheehongtang@hotmail.com>
2020-12-15 09:47:24 +0800

    Configure Presentation Layer for DependencyContainer

---
+++ Solution1/CH.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CH.Application.Interfaces;
+using CH.Application.Services;
+using CH.Domain.Interfaces;
+using CH.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CH.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- Solution1/CH.Mvc/Startup.cs
+++ Solution1/CH.Mvc/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CH.Infra.Data.Context;
+using CH.Infra.IoC;
 
 namespace CH.Mvc
 {
@@ -40,6 +41,8 @@ namespace CH.Mvc
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +74,11 @@ namespace CH.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
